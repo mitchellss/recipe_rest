ea41362a301fc95d8edc8117b727e14b962c7727
mitchellss <stephen.mitchell2299@gmail.com>
2021-05-17 10:54:43 -0400

    implement step assembler

--- src/main/java/us/stephenmitchell/recipe_rest/controller/StepController.java
+++ src/main/java/us/stephenmitchell/recipe_rest/controller/StepController.java
@@ -1,13 +1,22 @@
 package us.stephenmitchell.recipe_rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import us.stephenmitchell.recipe_rest.assembler.IngredientAssembler;
+import us.stephenmitchell.recipe_rest.assembler.StepAssembler;
+import us.stephenmitchell.recipe_rest.exception.IngredientNotFoundException;
 import us.stephenmitchell.recipe_rest.exception.StepNotFoundException;
+import us.stephenmitchell.recipe_rest.model.Ingredient;
 import us.stephenmitchell.recipe_rest.model.Step;
 import us.stephenmitchell.recipe_rest.repository.StepRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/api")
@@ -15,10 +24,22 @@ public class StepController {
 
     @Autowired
     StepRepository stepRepository;
+    StepAssembler stepAssembler;
+
+    StepController(StepRepository stepRepository, StepAssembler stepAssembler) {
+        this.stepRepository = stepRepository;
+        this.stepAssembler = stepAssembler;
+    }
 
     @GetMapping("/get_step")
-    public Iterable<Step> all() {
-        return stepRepository.findAll();
+    public CollectionModel<EntityModel<Step>> all() {
+        List<EntityModel<Step>> steps = StreamSupport
+                .stream(stepRepository.findAll().spliterator(), false)
+                .map(stepAssembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(steps,
+                linkTo(methodOn(StepController.class).all()).withSelfRel());
+
     }
 
     @PostMapping("/post_step")
@@ -32,9 +53,6 @@ public class StepController {
         Step step = stepRepository.findById(id)
                 .orElseThrow(() -> new StepNotFoundException(id));
 
-        return EntityModel.of(step,
-                linkTo(methodOn(StepController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(StepController.class).all()).withRel("step_list"),
-                linkTo(methodOn(RecipeController.class).one(step.getRecipe().getId())).withRel("recipe"));
+        return stepAssembler.toModel(step);
     }
 }
