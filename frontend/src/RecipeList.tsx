bb85c8fbdb2d685c5686fb7ec25524fb5e77df51
mitchellss <stephen.mitchell2299@gmail.com>
2022-03-17 20:02:46 +0000

    add basic recipe requesting

--- frontend/src/RecipeList.tsx
+++ frontend/src/RecipeList.tsx
@@ -1,12 +1,35 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function RecipeList() {
+    const [data, setData] = useState<any[]>([]);
+
+    const getAllData = () => {
+        axios.get("http://192.168.1.125:8080/api/recipe")
+            .then(res => {
+                setData(res.data);
+                console.log(res.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     useEffect(() => {
-        axios.get("https://")
-    });
-    return ( <div>Test</div> );
+        getAllData();
+    }, []);
+
+    return (
+    <div>
+        {data ?
+            data.map(test => {
+                return(
+                    <div key={test.id}>{test.title}</div>
+                )
+            }) : <div>No data yet</div>
+        }
+    </div>
+    );
 }
 
 export default RecipeList;
