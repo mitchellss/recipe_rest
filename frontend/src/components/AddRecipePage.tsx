1ba8ab16d3e72314d73c16a9464b118ed6cb185c
mitchellss <stephen.mitchell2299@gmail.com>
2022-05-06 14:06:05 -0400

    add ability to add recipe steps in ui

--- frontend/src/components/AddRecipePage.tsx
+++ frontend/src/components/AddRecipePage.tsx
@@ -2,25 +2,50 @@ import { FunctionComponent, useState } from "react";
 
 interface AddRecipePageProps {
 }
- 
-const AddRecipePage : FunctionComponent<AddRecipePageProps> = () => {
-    const [stepNums, setStepNums] = useState<number[]>();
+
+const AddRecipePage: FunctionComponent<AddRecipePageProps> = () => {
+    const [steps, setSteps] = useState<JSX.Element[]>();
+    const [stepNum, setStepNum] = useState<number>(0);
+
+    const clickAddIngredient = () => {
+        console.log("click")
+    }
+
+    const clickDeleteStep = (stepNum: number) => {
+
+    }
+
+    const clickAddStep = () => {
+        const new_step =             
+        <li key={`step_${stepNum}`}>
+            <label htmlFor={`step_${stepNum}`}>Step Text:</label>
+            <input type="text" name={`step_${stepNum}`} />
+            <input type="button" value="delete" onClick={() => clickDeleteStep(stepNum)}/>
+        </li>
+
+        steps?
+        setSteps([...steps, new_step])
+        : setSteps([new_step])
+        
+        setStepNum(stepNum + 1)
+    }
+
     return (
         <form>
             <label htmlFor="recipe_name">Recipe Title:</label>
             <input type="text" id="recipe_name" name="recipe_name" />
-            <br/>
+            <br />
             <label htmlFor="recipe_author">Author:</label>
             <input type="text" id="recipe_author" name="recipe_author" />
-            <br/>
+            <br />
             <label htmlFor="active_time">Active Time:</label>
             <input type="number" id="active_time" name="active_time" step="1" pattern="\d+" />
             <label>minutes</label>
-            <br/>
+            <br />
             <label htmlFor="total_time">Total Time:</label>
             <input type="number" id="total_time" name="total_time" step="1" pattern="\d+" />
             <label>minutes</label>
-            <br/>
+            <br />
             <label htmlFor="serves_low">Serves (# of People):</label>
             <input type="number" id="fname" name="serves_low" step="1" pattern="\d+" />
             <label htmlFor="serves_high">to</label>
@@ -29,40 +54,33 @@ const AddRecipePage : FunctionComponent<AddRecipePageProps> = () => {
             {
                 <div>
                     <label>Ingredient:</label>
-                    <input type="text" id="fname" name="fname"/>
+                    <input type="text" id="fname" name="fname" />
                     <label>Amount:</label>
-                    <input type="number" step="0.125" id="fname" name="fname"/>
+                    <input type="number" step="0.125" id="fname" name="fname" />
                     <select name="unit_dropdown" id="1">
                         <option value="cup">Cup(s)</option>
                         <option value="tbsp">Tablespoon(s)</option>
                         <option value="tsp">Teaspoon(s)</option>
                     </select>
-                    <br/>
+                    <br />
                     <label>Adjectives (i.e. sliced, diced, packed, sifted):</label>
-                    <input type="text" id="fname" name="fname"/>
-                    <br/>
+                    <input type="text" id="fname" name="fname" />
+                    <br />
                     <label>Additional Notes:</label>
-                    <input type="text" id="fname" name="fname"/>
+                    <input type="text" id="fname" name="fname" />
                 </div>
             }
-            <input type="button" value="Add Ingredient"/>
-            <br/>
+            <input type="button" value="Add Ingredient" onClick={clickAddIngredient} />
+            <br />
             <h2>Steps:</h2>
             <ol>
                 {
-                stepNums?  
-                    stepNums.map((item,index) => {
-                        return(<li key={`step_${index}`}>
-                            <label htmlFor={`step_${index}`}>Step Text:</label>
-                            <input type="text" name={`step_${index}`}/>
-                        </li>)
-                    })
-                    : <div></div>
+                    steps
                 }
             </ol>
-            <input type="button" value="Add Recipe Step"/>
+            <input type="button" value="Add Recipe Step" onClick={clickAddStep} />
         </form>
     );
 }
- 
-export default AddRecipePage ;
+
+export default AddRecipePage;
